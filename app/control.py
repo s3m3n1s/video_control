b1cf497fcd8a9e8d1ee8d193fda707bb57eb30f6
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2024-01-20 19:50:19 +0300

    Update control.py

--- app/control.py
+++ app/control.py
@@ -1,14 +1,4 @@
 from flask import request
 from app import app
 
-# import paho.mqtt.client as mqtt
 
-# Код для управления MQTT-клиентом
-# ...
-
-@app.route('/send_command', methods=['POST'])
-def send_command():
-    # Получение данных от джойстика и отправка команды по MQTT
-    # ...
-
-    return 'OK'
