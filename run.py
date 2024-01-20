9e44a86a6fb2b6a4342a244df27db503c47e24ba
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2024-01-20 23:23:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 import paho.mqtt.publish as publish
-
 from app import app
-
-
 
 mqtt_broker_host = "localhost"
 mqtt_broker_port = 1883
@@ -12,15 +9,24 @@ mqtt_topic = "turel/contol"
 
 @app.route('/send_command', methods=['POST'])
 def send_command():
-    try:
-        data = request.get_json()
+    data = request.get_json()
+    if 'down' in data:
+        try:
+            publish.single(mqtt_topic+"/horizontal", payload=str(data['right'] if data['right'] else data['left']),
+                           hostname=mqtt_broker_host, port=mqtt_broker_port)
+            publish.single(mqtt_topic+"/vertical", payload=str(data['up'] if data['up'] else data['down']),
+                           hostname=mqtt_broker_host, port=mqtt_broker_port)
+            return jsonify({"status": "success"})
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)})
+    elif 'shot' in data:
+        try:
+            publish.single(mqtt_topic+"/fire", payload=str(data['shot']),
+                           hostname=mqtt_broker_host, port=mqtt_broker_port)
+            return jsonify({"status": "success"})
+        except Exception as e:
+            return jsonify({"status": "error", "message": str(e)})
 
-        # Отправка данных в MQTT брокер
-        publish.single(mqtt_topic, payload=str(data), hostname=mqtt_broker_host, port=mqtt_broker_port)
-
-        return jsonify({"status": "success"})
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)})
 
 
 if __name__ == '__main__':
