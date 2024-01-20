b8c7548a767ffd66e2aa960cad97799b9e7759b9
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2024-01-20 19:49:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,27 @@
+from flask import Flask, request, jsonify
+import paho.mqtt.publish as publish
+
 from app import app
+
+
+
+mqtt_broker_host = "localhost"
+mqtt_broker_port = 1883
+mqtt_topic = "turel/contol"
+
+
+@app.route('/send_command', methods=['POST'])
+def send_command():
+    try:
+        data = request.get_json()
+
+        # Отправка данных в MQTT брокер
+        publish.single(mqtt_topic, payload=str(data), hostname=mqtt_broker_host, port=mqtt_broker_port)
+
+        return jsonify({"status": "success"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)})
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
