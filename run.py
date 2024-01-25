81c570ef8bd38d52f494c8ce7a25f9a77795a574
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2024-01-25 13:06:02 +0300

    Optimize mqtt

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, jsonify
 import paho.mqtt.publish as publish
+import paho.mqtt.client as client
 from app import app
 
 mqtt_broker_host = "localhost"
 mqtt_broker_port = 1883
 mqtt_topic = "turel/contol"
+
+
+mqtt_client = client.Client()
+mqtt_client.connect(mqtt_broker_host, mqtt_broker_port, 60)
+mqtt_send = mqtt_client.publish
 
 
 @app.route('/send_command', methods=['POST'])
@@ -13,17 +19,13 @@ def send_command():
     if 'down' in data:
         try:
             if data['right']:
-                publish.single(mqtt_topic + "/horizontal/right", payload=str(data['right']),
-                               hostname=mqtt_broker_host, port=mqtt_broker_port)
+                mqtt_send(mqtt_topic + "/horizontal/right", payload=str(data['right']))
             if data['left']:
-                publish.single(mqtt_topic + "/horizontal/left", payload=str(data['left']),
-                               hostname=mqtt_broker_host, port=mqtt_broker_port)
+                mqtt_send(mqtt_topic + "/horizontal/left", payload=str(data['left']))
             if data['up']:
-                publish.single(mqtt_topic + "/vertical/up", payload=str(data['up']),
-                               hostname=mqtt_broker_host, port=mqtt_broker_port)
+                publish.single(mqtt_topic + "/vertical/up", payload=str(data['up']))
             if data['down']:
-                publish.single(mqtt_topic + "/vertical/down", payload=str(data['down']),
-                               hostname=mqtt_broker_host, port=mqtt_broker_port)
+                publish.single(mqtt_topic + "/vertical/down", payload=str(data['down']))
 
             return jsonify({"status": "success"})
         except Exception as e:
