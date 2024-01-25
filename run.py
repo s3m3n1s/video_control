95536707c188941832f94bd870e2443d91d9b357
Saidumarov Semyen <74598420+s3m3n1s@users.noreply.github.com>
2024-01-25 15:32:06 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from flask import request, jsonify
 import paho.mqtt.client as client
 from app import app
+from time import time
+
+LATENCY = 0.10
+prev_time = time()
 
 mqtt_broker_host = "localhost"
 mqtt_broker_port = 1883
@@ -16,21 +20,28 @@ mqtt_client.connect(mqtt_broker_host, mqtt_broker_port, 60)
 def send_command():
     data = request.get_json()
     if 'down' in data:
-        try:
-            if data['right']:
-                mqtt_client.publish(mqtt_topic + "/horizontal/right", payload=str(data['right']))
-            if data['left']:
-                mqtt_client.publish(mqtt_topic + "/horizontal/left", payload=str(data['left']))
-            if data['up']:
-                mqtt_client.publish(mqtt_topic + "/vertical/up", payload=str(data['up']))
-            if data['down']:
-                mqtt_client.publish(mqtt_topic + "/vertical/down", payload=str(data['down']))
+        global prev_time
 
-            return jsonify({"status": "success"})
-        except Exception as e:
-            mqtt_client.reconnect()
-            # mqtt_client.connect(mqtt_broker_host, mqtt_broker_port, 60)
-            return jsonify({"status": "error", "message": str(e)})
+        if time() - prev_time > LATENCY:
+            prev_time = time()
+            try:
+                if data['right']:
+                    mqtt_client.publish(mqtt_topic + "/horizontal/right", payload=str(data['right']))
+                if data['left']:
+                    mqtt_client.publish(mqtt_topic + "/horizontal/left", payload=str(data['left']))
+                if data['up']:
+                    mqtt_client.publish(mqtt_topic + "/vertical/up", payload=str(data['up']))
+                if data['down']:
+                    mqtt_client.publish(mqtt_topic + "/vertical/down", payload=str(data['down']))
+
+                return jsonify({"status": "success"})
+            except Exception as e:
+                mqtt_client.reconnect()
+                # mqtt_client.connect(mqtt_broker_host, mqtt_broker_port, 60)
+                return jsonify({"status": "error", "message": str(e)})
+        else:
+            return jsonify({"status": "wait latency", "message": str(time() - prev_time)})
+
     elif 'shot' in data:
         try:
             mqtt_client.publish(mqtt_topic + "/fire", payload=str(data['shot']))
